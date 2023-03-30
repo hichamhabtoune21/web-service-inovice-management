80b2fb0759daca6afe7ec2a290188d18aad77112
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-03-30 19:43:37 +0200

    #

--- routes/users/list.js
+++ routes/users/list.js
@@ -1,8 +1,15 @@
 const api = require('../../lib/api-service');
 const router = api.Router();
+const db_methods=require('../../lib/db_methods');
+const User=require('../../lib/models/user').User;
 
-router.get("/", function (req, res) {
-    res.send({users: "test"});
-  });
+router.get("/", async function (req, res) {
+  try {
+    const users = await db_methods.findAll(User);
+    res.send(users);
+  } catch (error) {
+    console.error('Failed to retrieve data : ', error);
+  }
+});
 
 module.exports = router;
