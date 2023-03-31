dfcf829a6d833ed43027b8d5563c6fd1015c2f93
hichamhabtoune21 <99260356+hichamhabtoune21@users.noreply.github.com>
2023-03-31 11:57:54 +0200

    Update list.js

--- routes/invoices/list.js
+++ routes/invoices/list.js
@@ -4,10 +4,19 @@ const db_methods = require('../../lib/db_methods');
 const Invoice = require('../../lib/models/invoice').Invoice;
 
 router.get("/", async function (req, res) {
-  
   try {
     const invoices = await db_methods.findAll(Invoice);
     res.send(invoices);
+  } catch (error) {
+    console.error('Failed to retrieve data : ', error);
+  }
+});
+
+router.post("/create", async function (req, res) {
+  try {
+    //db_methods.Create(Invoice,req);
+    console.log(req.body);
+    res.send({text: 'success'})
   } catch (error) {
     console.error('Failed to retrieve data : ', error);
   }
