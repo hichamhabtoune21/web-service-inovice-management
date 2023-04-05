6b05244d65b217102fb1900b590ada2bc5fabd00
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-04-05 19:28:49 +0200

    some fixies in db_methods and updating example.test

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 require('dotenv').config()
-const api = require('./lib/api-service');
-const app=api.app;
-api.init();
+const app = require('./lib/app').app;
+const db= require('./lib/connect');
+db.connectToDb();
 
 const port = process.env.PORT;
-
-const health = require("./routes/health");
-const invoices = require("./routes/invoices/main");
-const listUsers = require("./routes/users/list");
-
-app.use("/health",health);
-app.use("/",health);
-app.use("/invoices",invoices);
-app.use("/users",listUsers);
 
 app.listen(port, async() => {
   console.log(`Example app listening on port ${port}`)
