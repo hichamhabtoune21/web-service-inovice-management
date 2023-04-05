d64948034b439406e1301497f475631f9c3de7e7
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-04-05 19:10:30 +0200

    #

--- tests/example.test.js
+++ tests/example.test.js
@@ -12,7 +12,7 @@ describe('Invoice tests', () => {
   test('should respond with 200 status code', async () => {
     await request(app)
     .get('/invoices/list')
-    .set('Authorization: Basic YWRtaW5AYWRtaW4uY29tOmFkbWluOg==')
+    .set('Authorization', 'Basic ' + 'YWRtaW5AYWRtaW4uY29tOmFkbWluOg==') 
     .expect(200)
      })
 })
